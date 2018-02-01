b96ed8b08180e7b52483303f8310bb5d1297fe26
Andrey Brizhak <andrewbrizhak@gmail.com>
2018-02-01 10:50:11 +0300

    init

--- WpfAppAllCode/Program.cs
+++ WpfAppAllCode/Program.cs
@@ -3,13 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
 
 namespace WpfAppAllCode
 {
-    class Program
+    // В этом первом примере определяется один класс для 
+    // представления самого приложения и главного окна.
+
+    class Program : Application
     {
+        [STAThread]
+
         static void Main(string[] args)
         {
+            // Обработать события Startup и Exit и затем запустить приложение.
+
+
         }
     }
 }
